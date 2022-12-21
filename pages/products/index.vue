4e1fedfa7f7471c4646de9d11b32c661a9af60fb
ali <alineamati.1381@gmail.com>
2022-12-21 12:56:39 +0330

    banner updated

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,27 +1,39 @@
 <template>
-  <div>
-    <div class="col-md-9 col-12 ml-auto mr-auto banner mt-5 pt-4 p-0">
+  <div class="">
+    <div class=" col-12 banner mt-5 pt-4 p-0">
       <img src="img/banner.jpg" class="rounded w-100 h-100" />
     </div>
-    <div class="p-md-5 p-4">
-      <h2 class="mt-3 text-center h1">All Products</h2>
-      <div class="mt-5 text-center">
-        <select class="col-3">
-          <option value="all">All Lamp</option>
-          <option value="study">Study Lamp</option>
-          <option value="kitchen">Kitchen Lamp</option>
-          <option value="livingRoom">Living Room Lamp</option>
-        </select>
-      </div>
-    </div>
 
-    <div class="d-flex flex-wrap justify-content-center">
-      <Card
-        v-for="product in products"
-        :key="product.id"
-        :lamp="product"
-        class="ml-md-4 mr-md-4 mt-md-4 col-md-2 col-5 mr-md-0 ml-md-0 mr-2 ml-2"
-      />
+    <div class="p-md-5 p-4"> 
+      <div class="p-md-5 p-4">
+        <h2 class="text-center h1">All Products</h2>
+        <div class="mt-5 text-center">
+          <select class="col-md-3 col-10">
+            <option value="all">All Lamp</option>
+            <option value="study">Study Lamp</option>
+            <option value="kitchen">Kitchen Lamp</option>
+            <option value="livingRoom">Living Room Lamp</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="d-flex flex-wrap justify-content-center">
+        <Card
+          v-for="product in products"
+          :key="product.id"
+          :lamp="product"
+          class="
+            ml-md-4
+            mr-md-4
+            mt-md-4
+            col-md-2 col-5
+            mr-md-0
+            ml-md-0
+            mr-2
+            ml-2
+          "
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -63,7 +75,13 @@ select {
 }
 
 .banner {
-  height: 200px;
-  filter: brightness(0.8);
+  height: 250px;
+  filter: brightness(0.3);
+}
+
+@media(max-width: 767px) {
+  .banner{
+    height: 200px;
+  }
 }
 </style>
